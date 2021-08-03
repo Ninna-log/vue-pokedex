b018de6dc1e6cbf008b4650b373c33b398a46273
Gianinna <gianinna.marquez.contractor@gdevtools.com>
2021-08-02 23:40:39 -0300

    Defining card component

---
+++ src/components/Card.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="card">
+        <div class="name">
+            <h2>Bulbasaur</h2>
+        </div>
+        <div class="star">
+            <img src="@/assets/ellipse-20.png" alt="ellipse">
+            <img src="@/assets/vector.png" alt="vector">
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: "Card",
+  data() {
+    return {
+      
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
